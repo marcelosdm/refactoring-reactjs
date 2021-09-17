eb06ec19b2f7dedc9d2c80cd0f8a8a37a0af3e16
Marcelo Melo <marcelomelo.ciandt@sulamerica.com.br>
2021-09-16 23:57:24 -0300

    Refactor Icon component

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
